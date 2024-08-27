accf5b1e4d05e68dd7f3b8a1d2b04cac088435ac
Amardurai <amardurai001@gmail.com>
2024-08-27 23:37:03 +0530

    Reminder listing screen design partially done

---
+++ app/src/main/java/com/example/reminderapp/presentatation/ReminderListScreen.kt
@@ -0,0 +1,50 @@
+package com.example.reminderapp.presentatation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ReminderListScreen() {
+    val isTimePickerVisible = remember {
+        mutableStateOf(false)
+    }
+    Scaffold(
+        topBar = { ReminderListTopAppBar() }
+    ) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReminderListTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = { Text(text = "Reminders") },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewReminderListScreen() {
+    ReminderListScreen()
+}
