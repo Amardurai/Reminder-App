7692afea4957c851205cae768b823e66f880434f
Amardurai <amardurai001@gmail.com>
2024-09-02 21:36:48 +0530

    Add ReminderReceiver class for handling meeting reminders and notifications

--- app/src/main/java/com/example/reminderapp/utils/ReminderReceiver.kt
+++ app/src/main/java/com/example/reminderapp/utils/ReminderReceiver.kt
@@ -1,33 +1,101 @@
 package com.example.reminderapp.utils
 
-import android.app.Notification
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.example.reminderapp.CHANNEL_ID
 import com.example.reminderapp.R
 import com.example.reminderapp.domain.model.MeetingReminder
+import com.example.reminderapp.domain.usecases.UpdateReminderUseCase
 import com.google.gson.Gson
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ReminderReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent?) {
-        val reminderJson = intent?.getStringExtra(AlarmUtils.REMINDER)
+
+    @Inject
+    lateinit var updateReminderUseCase: UpdateReminderUseCase
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val reminderJson = intent.getStringExtra(AlarmUtils.REMINDER)
         val reminder = Gson().fromJson(reminderJson, MeetingReminder::class.java)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    context,
-                    android.Manifest.permission.POST_NOTIFICATIONS
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                val notification = Notification.Builder(context, CHANNEL_ID)
+        val donePendingIntent = createPendingIntent(
+            context,
+            reminderJson,
+            Constants.DONE,
+            reminder.timeInMillis.toInt()
+        )
+        val cancelPendingIntent = createPendingIntent(
+            context,
+            reminderJson,
+            Constants.REJECT,
+            reminder.timeInMillis.toInt()
+        )
+
+
+        when (intent.action) {
+            Constants.DONE -> {
+                runBlocking {
+                    updateReminderUseCase.invoke(reminder.copy(isCompleted = true))
+                    AlarmUtils.cancelAlarm(context, reminder)
+                }
+            }
+
+            Constants.REJECT -> {
+                runBlocking {
+                    updateReminderUseCase.invoke(reminder.copy(isCompleted = true))
+                    AlarmUtils.cancelAlarm(context, reminder)
+                }
+            }
+
+            else -> {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        android.Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
+
+                val notification = NotificationCompat.Builder(context, CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_launcher_background)
                     .setContentTitle(reminder.title)
                     .setContentText(reminder.description)
+                    .addAction(R.drawable.baseline_check_24, "Done", donePendingIntent)
+                    .addAction(R.drawable.baseline_close_24, "Cancel", cancelPendingIntent)
+                    .build()
+
+                NotificationManagerCompat.from(context).notify(1, notification)
             }
         }
+
     }
+
+    private fun createPendingIntent(
+        context: Context,
+        reminderJson: String?,
+        action: String,
+        requestCode: Int
+    ): PendingIntent {
+        val intent = Intent(context, ReminderReceiver::class.java).apply {
+            putExtra(AlarmUtils.REMINDER, reminderJson)
+            this.action = action
+        }
+        return PendingIntent.getBroadcast(
+            context, requestCode, intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+
 }
